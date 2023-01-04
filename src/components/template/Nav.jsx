dc01023ac593ec052d33e3d5c0748723258aeafe
Daniel Lim-Apo <daniel.github@sede.com.br>
2023-01-04 09:37:30 -0300

    Added navigation with Routes from react-router-dom and changed a href to Link to from react-router-dom

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <a href = "#/">
+            <Link to= "/">
                 <i className="fa fa-home"> Start </i>
-            </a>
-            <a href = "#/users">
+            </Link>
+            <Link to= "/users">
                 <i className="fa fa-home"> Users </i>
-            </a>
+            </Link>
         </nav>
     </aside>
