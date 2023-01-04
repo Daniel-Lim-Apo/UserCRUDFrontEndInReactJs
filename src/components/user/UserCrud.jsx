031d3ad9127a5afcf8e2e43d6b3d563a74375633
Daniel Lim-Apo <daniel.github@sede.com.br>
2023-01-04 10:15:33 -0300

    Initial setup for use axios to add a new user added in UserCrud.jsx

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Main from "../template/Main";
+import axios from "axios";
+import Settings from '../../main/config/Settings'
 
 const headerProps = {
     icon: 'users',
@@ -7,7 +9,42 @@ const headerProps = {
     subtitle: 'Users CRUD: Create, Read, Update, Delete'
 }
 
+const baseUrl = Object.freeze('https://localhost:3011/users');
+
+const initialState = {
+    user: {
+        name: '',
+        email: ''
+        },
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user });
+    }
+
+    save(){
+        const user = this.state.user;
+        const method = user.id ? 'put': 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data);
+                this.setState({ user: initialState.user, list });
+            })
+    }
+
+    getUpdatedList(user){
+        // Create a new list without the user  
+        const list = this.state.list.filter(u => u.id !== user.id)
+        // Then put the user at the start of the list
+        list.unshift(user)
+    }
+
     render() {
         return (
             <Main {...headerProps}>
