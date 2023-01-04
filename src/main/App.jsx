16964f4c8a0d11f6acaa03288cffbc571d19157e
Daniel Lim-Apo <daniel.limapo@pcdf.df.gov.br>
2023-01-03 21:35:04 -0300

    Added code with css bootstrap and some components in a initial form

---
+++ src/main/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+import Logo from  '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+export default props => 
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main
+            icon="home"
+            title="Start"
+            subtitle="React User CRUD Sample"
+        />
+        <Footer />
+    </div>
